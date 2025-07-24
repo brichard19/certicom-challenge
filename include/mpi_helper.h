5133d9de795a988bf688312a824d39b9d54340c3
brichard19 <9160057+brichard19@users.noreply.github.com.>
2025-07-24 12:56:50 -0400

    Move MPI_CALL macro to mpi_helper.h

---
+++ include/mpi_helper.h
@@ -0,0 +1,16 @@
+#ifndef _MPI_HELPER_H
+#define _MPI_HELPER_H
+
+#include <mpi.h>
+
+#define MPI_CALL(condition)\
+{\
+  int err = condition;\
+  if(err != MPI_SUCCESS) {\
+    std::cout << "MPI error " << err << " " << __LINE__ << std::endl;\
+    MPI_Finalize();\
+    exit(1);\
+  }\
+}\
+
+#endif
--- main.cpp
+++ main.cpp
@@ -20,19 +20,8 @@
 #include "mont.h"
 
 #ifdef BUILD_MPI
-
 #include <mpi.h>
-
-#define MPI_CALL(condition)\
-{\
-  int err = condition;\
-  if(err != MPI_SUCCESS) {\
-    std::cout << "MPI error " << err << " " << __LINE__ << std::endl;\
-    MPI_Finalize();\
-    exit(1);\
-  }\
-}\
-
+#include "mpi_helper.h"
 #endif
 
 
