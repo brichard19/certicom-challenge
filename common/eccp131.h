0b9bdec2a23d02a4d1d9c5da08f2f637a0773014
brichard19 <9160057+brichard19@users.noreply.github.com.>
2025-08-23 13:58:24 -0400

    Further refactor code
    Move paramter definitions for P79 and P131 into montgomery.cpp

--- common/eccp131.h
+++ common/eccp131.h
@@ -5,9 +5,28 @@
 
 #include "uint131.h"
 
-#include "p131.h"
-#include "p79.h"
+struct CurveParameters {
+    uint131_t p;
+    uint131_t a;
+    uint131_t b;
+    uint131_t n;
+    uint131_t gx;
+    uint131_t gy;
+    uint131_t qx;
+    uint131_t qy;
+
+    uint131_t k;
+    uint131_t one;
+    uint131_t p_minus_2;
+    uint131_t sqrt;
+    uint131_t r;
+    uint131_t r2;
+    int bits;
+    int words;
+    std::string name;
+};
 
 extern CurveParameters _params;
+
 
 #endif
