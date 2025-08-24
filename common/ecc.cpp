5da1a6d836b128d5b11d4694d0994f99a351a1ff
brichard19 <9160057+brichard19@users.noreply.github.com.>
2025-08-23 22:31:30 -0400

    Move EC parameters declaration into ecc.cpp

--- common/ecc.cpp
+++ common/ecc.cpp
@@ -4,6 +4,99 @@
 #include "eccp131.h"
 #include "montgomery.h"
 #include "util.h"
+#include <stdexcept>
+
+
+CurveParameters _eccp131 = {
+    .p = {{0x194c43186b3abc0b, 0x8e1d43f293469e33, 0x4}},
+    .a = {{0xe7f7f250cee8709a, 0xacd15fe1a8ec1522, 0x00}},
+    .b = {{0xc85087e5ab4eca9e, 0xde124657d7ba5851, 0x2}},
+    .n = {{0x7f7ed728f6b8e6f1, 0x8e1d43f293469e31, 0x4}},
+
+    .gx = {{0xb137748018df6458, 0xb188ba8cd2a386d9, 0x00}},
+    .gy = {{0x4b5a2a8f9b90cbef, 0x7dabb39a1bb0a5fa, 0x2}},
+
+    .qx = {{0xc83af1fe332475e3, 0xe38a3357a4b0bb01, 0x2}},
+
+    .qy = {{0xfe97756ed241b570, 0xb167247624e73021, 0x3}},
+
+    // k such that k * k^-1 = -1 (mod R)
+    // k = (r * r_inv -1) // p
+    .k = {{0xe0587d72985b105d, 0xf1fd54b0309e1ab9, 0x7cfd70cf}},
+
+    .one = {{0x6e7743da32b6d0c7, 0x88c614d64c1a8f0b, 0x00}},
+
+    .p_minus_2 = {{0x194c43186b3abc09, 0x8e1d43f293469e33, 0x4}},
+
+    // (P + 1) / 4
+    .sqrt = {{0xc65310c61aceaf03, 0x238750fca4d1a78c, 0x1}},
+
+    // R mod p
+    .r = {{0x6e7743da32b6d0c7, 0x88c614d64c1a8f0b, 0x00}},
+
+    // R^2
+    .r2 = {{0xf95d709f92600513, 0xf3d6fa1fb65ef639, 0x3}},
+
+    .bits = 131,
+
+    .words = (131 + 63) / 64,
+
+    .name = "ecp131"
+};
+
+CurveParameters _eccp79 = {
+  .p = {{0x5177412aca899cf5, 0x62ce, 0x00}},
+
+  .a = {{0x732c9b460e3c3d, 0x1bb7, 0x00}},
+
+  .b = {{0xc88edfd7d5b44610, 0x250c, 0x00}},
+
+  .n = {{0x5177407b7258dc31, 0x62ce, 0x00}},
+
+  .gx = {{0x8fa818f2b62053c8, 0x3e37, 0x00}},
+
+  .gy = {{0xddab9a8daa5aa60b, 0x21a9}},
+
+  .qx = {{0x96642c5fb8dbd341, 0x7a7}},
+
+  .qy = {{0xbf3614d658e2931c, 0x426c}},
+
+  .k = {{0x6e3655426732d0a3, 0xcafea9fd045a89b6, 0xe6bb05ec}},
+
+  .one = {{0x5447aa703f6abc5f, 0x1358, 0x00}},
+
+  .p_minus_2 = {{0x5177412aca899cf3, 0x62ce, 0x00}},
+
+  // (p - 5) / 8
+  .sqrt = {{0xca2ee8255951339e, 0xc59, 0x00}},
+  
+  .r = {{0x5447aa703f6abc5f, 0x1358, 0x00}},
+
+  // R^2
+  .r2 = {{0x7b0baef57de52417, 0xe79, 0x00}},
+
+  .bits = 79,
+
+  .words = (79 + 63) / 64,
+
+  .name = "ecp79"
+};
+
+CurveParameters _params;
+
+namespace ecc {
+void set_curve(const std::string& curve_name)
+{
+  if(curve_name == "ecp131") {
+    _params = _eccp131;
+  } else if(curve_name == "ecp79") {
+    _params = _eccp79;
+  } else {
+    throw std::runtime_error("Invalid curve name");
+  }
+}
+
+}
 
 namespace {
 
