11dad28996726406f9890d19cd4b1e6634760abf
brichard19 <9160057+brichard19@users.noreply.github.com.>
2025-08-25 21:10:18 -0400

    Move some headers to include directory

---
+++ include/montgomery.h
@@ -0,0 +1,23 @@
+#ifndef _MONTGOMERY_MATH_H
+#define _MONTGOMERY_MATH_H
+
+#include "uint131.h"
+
+
+namespace mont {
+
+uint131_t to(uint131_t a);
+uint131_t from(uint131_t a);
+uint131_t add(uint131_t a, uint131_t b);
+uint131_t neg(uint131_t a);
+uint131_t sub(uint131_t a, uint131_t b);
+uint131_t mul(uint131_t a, uint131_t b);
+uint131_t square(uint131_t a);
+uint131_t inv(uint131_t a);
+uint131_t sqrt(uint131_t x);
+uint131_t rshift(uint131_t x, int n);
+uint131_t lshift(uint131_t x, int n);
+
+};
+
+#endif
